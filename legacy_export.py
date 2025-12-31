35dde897d3d9c20557873b32bb5ee4b88fba1224
Nick Harding <nicholas.harding2@nhs.net>
2025-12-31 16:59:43 +0000

    legacy base

--- legacy_export.py
+++ legacy_export.py
@@ -2,6 +2,22 @@ def empty_legacy_base(sequence: int) -> dict:
     """
     Base legacy command with all required fields present.
     """
-    return{
-        "CommandSequence": sequence
+    return {
+        "CommandSequence": sequence,
+        "Directive": 0,
+        "UniformMargin": 0.0,
+        "X1": 0.0,
+        "X2": 0.0,
+        "Y1": 0.0,
+        "Y2": 0.0,
+        "Z1": 0.0,
+        "Z2": 0.0,
+        "OriginalID": "",
+        "SecondID": "",
+        "NewID": "",
+        "ROIType": 0,
+        "ReadableCmd": "",
+        "Expand": False,
+        "ValidIDs": False
     }
+
